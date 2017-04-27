78bd21981349dea93aad314177b0f8b532a886eb
romeopatrick11 <hello@romeopatrick.com>
2017-04-26 21:36:01 -0400

    accessing arguments

--- ch03/01/Main.java
+++ ch03/01/Main.java
@@ -1,5 +1,5 @@
 public class Main{
 	public static void main(String[] args){
-		System.out.println("Hello World!\n Congrats you just wrote your first program");
+		System.out.println("The argument you passed in is:"+args[0]);
 	}
 }
